8f2bfc89d5f68704743933531435600f3e9901e7
besukes <besukesfn@gmail.com>
2026-04-06 20:16:27 +0100

    Add function declarations for game modules

--- simpleSimon.h
+++ simpleSimon.h
@@ -1,3 +1,6 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
 typedef int numCarta;
 
 typedef struct ultimaJogada{
@@ -13,3 +16,20 @@ typedef struct reverterJogada{
 typedef struct str{
     char str[20];
 }string;
+
+//modulo interfaceGrafica.c
+int escolhaDeResolucao();
+void atribuiResolucao(int * resX,int * resY,int optn);
+SDL_Renderer * sdl_initializer();
+void clean_sdl(SDL_Texture * image[]);
+
+//modulo criarJogo.c
+void preset(int cartas[]);
+void presetStr(string cartasStr[]);
+void baralhar(int cartas[],string cartasStr[]);
+void criarJogo(int matrizCartasJogo[10][21],SDL_Texture * imagensCartas[10][21]);
+
+//modulo funcoesBase.c
+int verificaVitoria(int * baralhosCompletos);
+int cartaPegavel(int cartaClique,int linhaMatriz,int matrizCartasJogo[10][21]);
+int cartaColocavel(int cartaDeBaixo,int cartaDeCima);
