b0eb2097862b54f04e074d08d45978e66426e5d3
besukes <besukesfn@gmail.com>
2026-04-07 12:55:18 +0100

    Refactor SDL initialization and function parameters

--- simpleSimon.c
+++ simpleSimon.c
@@ -4,21 +4,21 @@
 
 
 void interfaceJogo(int matrizCartasJogo[10][21],int baralhosCompletos[], undoMove estadoUndoGlobal,
-SDL_Texture * imagensCartas[10][21],SDL_Texture * imagensJogo[]){
+SDL_Texture * imagensCartas[10][21],SDL_Texture * imagensJogo[],SDL2Bases args){
     
 }
 
 
 int main(void){
-    SDL_Renderer * renderer = sdl_initializer();
+    SDL2Bases args = sdl_initializer();
     //precisamos de uma função que inicialize estes dois
     SDL_Texture* imagensJogo[10];
     SDL_Texture* imagensCartas[10][21];
     //o limite máximo teórico numa fila de cartas seria 21
     int matrizCartasJogo[10][21], baralhosCompletos[4]={0};
     undoMove estadoUndoGlobal = {0,{}};
-    criarJogo(matrizCartasJogo,imagensCartas,renderer);
-    interfaceJogo(matrizCartasJogo,baralhosCompletos,estadoUndoGlobal,imagensCartas,imagensJogo);
-    clean_sdl(imagensJogo);
+    criarJogo(matrizCartasJogo,imagensCartas,args.rendererBase);
+    interfaceJogo(matrizCartasJogo,baralhosCompletos,estadoUndoGlobal,imagensCartas,imagensJogo,args);
+    clean_sdl(imagensJogo,imagensCartas);
     return 0;
 }
