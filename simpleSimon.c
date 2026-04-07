6b66a99a083e0a036e5c5b8321e7db5fa17d5c6c
besukes <besukesfn@gmail.com>
2026-04-07 12:42:46 +0100

    Refactor main function and update texture arrays

--- simpleSimon.c
+++ simpleSimon.c
@@ -9,14 +9,15 @@ SDL_Texture * imagensCartas[10][21],SDL_Texture * imagensJogo[]){
 }
 
 
-int main(){
+int main(void){
     SDL_Renderer * renderer = sdl_initializer();
     //precisamos de uma função que inicialize estes dois
-    SDL_Texture * imagensJogo[10], * imagensCartas[10][21];
+    SDL_Texture* imagensJogo[10];
+    SDL_Texture* imagensCartas[10][21];
     //o limite máximo teórico numa fila de cartas seria 21
     int matrizCartasJogo[10][21], baralhosCompletos[4]={0};
     undoMove estadoUndoGlobal = {0,{}};
-    criarJogo(matrizCartasJogo,imagensJogo);
+    criarJogo(matrizCartasJogo,imagensCartas,renderer);
     interfaceJogo(matrizCartasJogo,baralhosCompletos,estadoUndoGlobal,imagensCartas,imagensJogo);
     clean_sdl(imagensJogo);
     return 0;
