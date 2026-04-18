6afeddd8d9c7f4a740a07988eddc06c4febe023d
Alberto <a115738@alunos.uminho.pt>
2026-04-18 20:28:24 +0100

    quase nada

--- src/interfaceGrafica.c
+++ src/interfaceGrafica.c
@@ -12,6 +12,7 @@ int escolhaDeResolucao(void){
     scanf("%d",&u);
     return u;
 }
+
 // A Função offsetloop retorna um valor entre 0 e dist que sobe e desce ciclicamente.
 // Tciclo serve para dizer a duração da interação toda , ou seja , se for 2 segundos e 2 ciclos então demora 1 segundo por cada ciclo.
 double offsetloop(int tempo , double TCiclo, int dist){
@@ -113,36 +114,27 @@ ate ao indice matrizJogo[col][0] que seria o número de cartas nessa mesma fila*
 void desenharCartas(int matrizJogo[10][21], SDL_Texture *imagensCartas[10][21], UserBase *args){
     int cartaW = 140, cartaH = 190, offsetX = 75, espacoX = 178, offsetY = 80, passo = 32;
     double offset = offsetloop(args->tempo, 2, 8);
-
     for (int col = 0; col < 10; col++) {
         for (int row = 1; row <= matrizJogo[col][0]; row++) {
-
             SDL_Rect dest = {
                 offsetX + col * espacoX,
                 offsetY + row * passo,
                 cartaW,
                 cartaH
             };
-
             int hovered = (
                 args->mouseX >= dest.x && args->mouseX <= dest.x + dest.w &&
                 args->mouseY >= dest.y && args->mouseY <= dest.y + dest.h
             );
-
             double ang = 0;
-
             if(args->numCartasSelecionadas == 0 && hovered && 
                cartaPegavel(matrizJogo[col][row], col, matrizJogo)){
 
-                int extraW = 20;
-                int extraH = 30;
-
+                int extraW = 20 , extraH=30;
                 dest.x -= extraW / 2;
                 dest.y -= extraH / 2 + offset;
-
                 dest.w += extraW;
                 dest.h += extraH;
-
                 // TILT FIXO para a direita
                 ang = 10;
             }
